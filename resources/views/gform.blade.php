7046b7e4220819b3d68f79ad477ff2bdc1f3709c
Theofilusarifin <theofilusarifin@gmail.com>
2021-09-25 05:47:49 +0700

    Update gform.blade.php

--- resources/views/gform.blade.php
+++ resources/views/gform.blade.php
@@ -4,7 +4,7 @@
     <div class="theme-inner-banner section-spacing">
         <div class="overlay">
             <div class="container">
-                <h2>Gform Prestasi</h2>
+                <h2>Info Mahasiswa</h2>
             </div>
         </div>
     </div>
@@ -13,11 +13,11 @@
         <div class="container">
             <div class="theme-title-one mb-5">
                 <h2>Gform Prestasi</h2>
+                <p class="mt-2">Klik button Fill Out Form untuk mengisi GForm</p>
             </div>
-                <div class="wrapper">
-                    <div class="row d-flex justify-content-center align-items-center">
-                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSew_ihB53B-cWRcmfB3J9jYM4QV_flg2efX9V0ygz9FDPO5Mg/viewform?embedded=true" width="800" height="950" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-                    </div>
+            <div class="wrapper">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeHobASgguFpM4LbZVHtLudsZEsn30TiEs4EtHB3K-tdnBm6A/viewform?embedded=true" width="800" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </div>
         </div>
