64d2eaffd48c5f1661856dd76823774575334911
Theofilusarifin <theofilusarifin@gmail.com>
2021-09-25 11:35:15 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin',
+            'password' => bcrypt('executiveengineers'),
+            'email' => 'bemftubaya@gmail.com'
+        ]);
     }
 }
