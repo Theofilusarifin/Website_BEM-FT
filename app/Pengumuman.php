3d6123f6b00af76bbe883e07e6fe4b6e84c84827
Theofilusarifin <theofilusarifin@gmail.com>
2021-10-22 22:59:55 +0700

    Paksa table ke pengumuman

--- app/Pengumuman.php
+++ app/Pengumuman.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengumuman extends Model
 {
+    protected $table = 'pengumumans';
     protected $fillable = [
         'judul', 'slug', 'created_at', 'isi', 'link_foto',
     ];
