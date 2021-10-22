ab25ec26cd7b6f86b691445365312f5fba287a34
reynardnathanael <jiangreynard@gmail.com>
2021-10-23 00:26:54 +0700

    index pengumuman

--- resources/views/pengumuman/index.blade.php
+++ resources/views/pengumuman/index.blade.php
@@ -9,7 +9,48 @@
 			</div>
 		</div>
 	</div>
+
 	@if($pengumumans->count())
+	<div class="blog-inner-page section-spacing">
+		<div class="container">
+			<div class="row">
+				@foreach($pengumumans as $pengumuman)
+				<div class="col-xl-9 col-lg-8 col-12 our-blog">
+					<div class="post-wrapper">
+						<div class="single-blog">
+							<div class="image-box">
+								<img src="{{ asset('storage/pengumuman/'.$pengumuman->judul.'.png') }}" alt="Foto Pengumuman">
+								<div class="overlay"><a href="#" class="date">Feb 06, 2018</a></div>
+							</div>
+							<div class="post-meta">
+								<h5><a href="{{ 'pengumuman/'.$pengumuman->slug }}">{{ $pengumuman->judul }}</a></h5>
+								<p>{{ $pengumuman->isi }}</p>
+								<a href="{{ 'pengumuman/'.$pengumuman->slug }}" class="read-more">READ MORE</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				@endforeach
+			</div>
+		</div>
+		{{-- Pagination --}}
+			<div class="d-flex justify-content-center">
+				<div>
+					{{ $pengumumans->links() }}
+				</div>
+			</div>
+	</div>
+	@else
+		<div class="our-solution section-spacing mt-5">
+			<div class="container">
+				<div class="theme-title-one">
+					<h4>Belum ada pengumuman untuk saat ini</h4>
+				</div>
+			</div>
+		</div>
+	@endif
+@endsection
+	<!--@if($pengumumans->count())
 		<div class="our-solution section-spacing mt-5">
 			<div class="container">
 				<div class="theme-title-one">
@@ -17,7 +58,10 @@
 				</div>
 			</div>
 		</div>
-		<div class="our-case our-project section-spacing">
+	-->
+	
+	
+		<!-- <div class="our-case our-project section-spacing">
 			<div class="container">
 				<div class="wrapper mb-4">
 					<div class="row">
@@ -46,8 +90,8 @@
 					</div>
 				</div>
 			</div>
-		</div>
-	@else
+		</div> -->
+	<!-- @else
 		<div class="our-solution section-spacing mt-5">
 			<div class="container">
 				<div class="theme-title-one">
@@ -55,5 +99,4 @@
 				</div>
 			</div>
 		</div>
-	@endif
-@endsection
+	@endif -->
